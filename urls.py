40b3125510f05e8db238e40a68bccf32528d3067
vvarunsenthil-cloud <vvarunsenthil@gmail.com>
2026-01-20 10:17:29 +0530

    Add URL patterns for course submissions

---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+app_name = 'onlinecourse'
+
+urlpatterns = [
+    path(
+        'course/<int:course_id>/submit/',
+        views.submit,
+        name='submit'
+    ),
+    path(
+        'course/<int:course_id>/submission/<int:submission_id>/result/',
+        views.show_exam_result,
+        name='exam_result'
+    ),
+]
